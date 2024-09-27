2a1d4e93b345777cc29af089b255b4775cc87a3a
orerevva <polina.oreva@mail.ru>
2024-09-27 23:28:51 +0200

    add: new file

---
+++ семестр 1/Laba4.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <clocale>
+int main ()
+{
+//1
+// int a,b,c;
+// std::cin >> a>>b>>c;
+// if ((a>b) && (b>c)){
+// std::cout<<(a-b-c)<<std::endl;
+// } else if ((b>a)&& (b%c==0)) {
+// std::cout<<((b/c)+b-a)<<std::endl;
+// }else {
+// std::cout<<(a+b+c)<<std::endl;    
+// }
+
+//2
+// int n;
+// std::cin>>n;
+// switch(n)
+// {
+
+//     case 0:
+//         std::cout<< "Everything is fine"<<std::endl;
+//         break;
+//     case 1:
+//         std::wcout<<"file reading error"<< std::endl;
+//         break;
+//     case 2:
+//         std::cout<<"file write error"<<std::endl;
+//         break;
+//     case 3:
+//         std::cout<<"not all fields are defined"<<std::endl;
+//         break;
+//     default:
+//     std::cout<<"erroneous input"<<std::endl;
+//     break;
+    
+
+
+// }
+
+//3
+int x;
+std::cin>>x;
+if (x==1){
+    std::cout<<"Positive number"<<std::endl;
+
+} else {
+    std::cout<<"Negative number"<<std::endl;
+}
+
+// int x;
+// std::cin>>x;
+// switch(x)
+// {
+//     case(-1):
+//         std::cout<<"Negative number"<<std::endl;
+//         break;
+//     case(1):
+//         std::cout<<"Positive number"<<std::endl;
+//         break;
+//     default:
+//         std::cout<<"Positive number"<<std::endl;
+// }
+return 0;
+
+} 
