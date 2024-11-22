719781e4bc764289298a0a05ba7cfd98fb568c9d
orerevva <polina.oreva@mail.ru>
2024-11-22 21:42:46 +0200

    Update main.cpp

--- семестр 1/Laba 8/main.cpp
+++ семестр 1/Laba 8/main.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 
 const int N_MAX=2000;
-// чтение
+
 bool Read (int& n, int& cnt, std::string words[N_MAX]) 
 {
     std::ifstream in("input.txt");
@@ -34,19 +34,19 @@ void Clear (int cnt, std::string words[N_MAX])
             }
         }
 }
-void Search(int& cnt, char m, int mas[N_MAX], std::string words[N_MAX], std::string new_words[N_MAX])
+void Search(int& cnt, char m, int mas[N_MAX], std::string words[N_MAX])
 {   
     int k=0;
     for (int i=0; i<cnt; ++i)
         for (int j=0; j<words[i].length(); j++)
             {   int k=0;
-                new_words[i]=tolower(words[i][j]);
-                if (new_words[i].find(m)==0)
+                tolower(words[i][j]);
+                if (words[i].find(m)==0)
                 {
-                    mas[i]= (new_words[i].length());
+                    mas[i]= (words[i].length());
                     continue;
                 }
-                new_words[k]=new_words[i];
+                words[k]=words[i];
                 k++;
             }
     cnt=k;
@@ -62,10 +62,10 @@ void Sorting (int cnt, int mas[N_MAX], std::string words[N_MAX])
                 std::swap(words[i], words[j]);
             }   
 }
-void wwrite(int cnt,std::string new_words[N_MAX])
+void wwrite(int cnt,std::string words[N_MAX])
 {
     for (int i=0; i<cnt; i++)
-        std::cout<<"<"<<new_words[i]<<">"<<std::endl;
+        std::cout<<"<"<<words[i]<<">"<<std::endl;
         
 }
 
@@ -94,10 +94,9 @@ int main ()
     if (!(Read (n, cnt, words)))
         return -1;
     Clear(cnt, words);
-    Search(cnt, m, mas, words, new_words);
-    //Sorting(cnt,mas, words);
-    //Write(cnt,n, words);
-    wwrite(cnt, new_words);
+    Search(cnt, m, mas, words);
+    Sorting(cnt,mas, words);
+    Write(cnt,n, words);
     return 0;
 }
 
@@ -105,75 +104,3 @@ int main ()
 
 
 
-/*
-bool Read (int&n, int& cnt, std::string words[N_MAX])
-{
-    std::ifstream in("input.txt");
-    if (!in.is_open())
-    {
-        std::cerr<<"File error"<<std::endl;
-        return false;
-    }
-    in>>n;
-    cnt=0; // сколько слов в массиве  
-    while (!in.eof())
-    {
-        in>> words[cnt];
-        cnt++;
-        //std::cout << "<"<< s<<">"<< std::endl;
-    }
-        return true;
-    }
-
-    void Write(int cnt, std::string words[N_MAX])
-{
-    for (int i=0;i<cnt;++i)
-        std::cout<<"<"<<words[i]<<">"<<std::endl;
-}
-
-void Write1(int n, int cnt, std::string words[N_MAX])
-{   
-    bool first=true;
-    for (int i=0;i<cnt;++i)
-        {   
-            if (n==0)
-                break;
-            n--;
-            if (first)
-            {
-                
-            }
-            std::cout<<"<"<<words[i]<<">"<<std::endl;
-        }
-}
-
-void ClearWords (int cnt, std::string words[N_MAX])
-{
-    for (int i=0; i<cnt;++i)
-        for (int j=0; j<words[i].length();++j)
-        {
-            if(!isalpha(words[i][j]))
-            {
-                words[i].erase(j,1);
-                --j;
-            }
-        }
-}
-
-int main()
-{   
-    int n;
-    int cnt=0;
-    std::string words[N_MAX];
-    if (!Read(n,cnt,words))
-        return -1;
-    ClearWords(cnt,words);
-    char m;
-    std::cin>> m;
-    Write(cnt,words);
-    return 0;
-}
-
-
-
-*/
